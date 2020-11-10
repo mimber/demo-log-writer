4bb866f84c7e7763c5866a156a2f59ba13cbad85
Mario Imber <mario.imber@apa.at>
2020-11-10 10:54:37 +0100

    Initialize demo app that generates log output

---
+++ src/main/java/at/apa/demo/logwriter/DemoLogWriterApplication.java
@@ -0,0 +1,13 @@
+package at.apa.demo.logwriter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLogWriterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLogWriterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/apa/demo/logwriter/DemoLogWriterApplicationTests.java
@@ -0,0 +1,13 @@
+package at.apa.demo.logwriter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLogWriterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
